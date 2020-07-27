93dd92b74d7efd903b2dd00200df61a149fde962
nickollasc <36551569+nickollasc@users.noreply.github.com>
2020-07-27 01:34:08 +0100

    Add files via upload

--- get_network_ips.sh
+++ get_network_ips.sh
@@ -4,7 +4,7 @@ if ! type nmap &> /dev/null; then
   test -e /etc/debian_version && sudo apt install -y nmap || sudo yum install -y nmap
 fi
 
-function get_ips {
+function get_ips_and_macs {
   lan_ip=$( ip route | awk '/default/ { split( $3, a, "." ); print a[1]"."a[2]"."a[3] }' )	  # get lan ip like 192.168.0
 
   sudo nmap -sn -PO ${lan_ip}.1-255								| # ping scan for a ip range 
@@ -13,13 +13,13 @@ function get_ips {
   awk '{ a[$1] = a[$1] FS substr( $0, index( $0,$2 ) ) } END{ for( i in a ) print i a[i] }'	  # print IP and MAC on same line
 } 
 
-get_ips									|
+get_ips_and_macs							|
 sort -k2n								| # numeric sort by column 2 IPs
 awk '$3!=""?$3:$3="_"'							| # fill IPs without MAC with _ 
 awk '$3=="68:FF:7B:6B:5C:D5"?$0=$0" Router":$0'				| # Router
-awk '$3=="DC:90:88:2E:8C:12"?$0=$0" MyMobile":$0'			| # Huawey Nick
+awk '$3=="DC:90:88:2E:8C:12"?$0=$0" Mobile":$0'				| # Mobile
 awk '$3=="40:CD:7A:25:3D:71"?$0=$0" TV":$0'				| # TV
 awk '$3=="DC:A6:32:3C:4C:C0"?$0=$0" Raspberry":$0'			| # Raspberry
-awk '$3=="_"?$0=$0" MyLaptop":$0'					| # My Laptop
+awk '$3=="_"?$0=$0" Laptop":$0'						| # Laptop
 awk '$3=="D0:33:11:35:B7:29"?$0=$0" \033[33;1mGuest-Out\033[m":$0'	| # Guest Out 
 column -t								  # format stdout
